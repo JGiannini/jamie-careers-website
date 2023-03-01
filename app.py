323ffa90e590ed05be15f0c8d156abcb8a85e3c0
JamieG <jgianninidesign@gmail.com>
2023-03-01 02:12:16 +0000

    Added HTML and CSS

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
-from flask import Flask
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-  return "Hello, Jamie!!!"
+  return render_template('home.html')
 
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0',
           debug=True)  #Lets us run it locally on local host 0.0.0.0
-
