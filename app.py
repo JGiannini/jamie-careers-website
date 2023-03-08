cdaf5e1f6ccefa0fd28479aad6530dbbfbbb64c0
JamieG <jgianninidesign@gmail.com>
2023-03-08 20:27:20 +0000

    Updated Python and added jobitem html page

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+#This is a Python dictionary:
+JOBS = [{
+  'id': 1,
+  'title': 'Data Analyst',
+  'location': 'Bengaluru, India',
+  'salary': 'Rs. 1,00,000'
+}, {
+  'id': 2,
+  'title': 'Data Scientist',
+  'location': 'San Francisco, USA',
+  'salary': '$150,000'
+}, {
+  'id': 3,
+  'title': 'Frontend Engineer',
+  'location': 'Remote',
+  'salary': '$120,000'
+}, {
+  'id': 4,
+  'title': 'Backend Engineer',
+  'location': 'Remote',
+}]
 
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, company_name='Jamie')
 
 
 if __name__ == '__main__':
